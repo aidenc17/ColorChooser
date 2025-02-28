e441ebb869cf4bc3f420a171372236ef2c280eaa
aiden cox <cox.378@wright.edu>
2025-02-28 13:18:05 -0500

    bad news. drop down works but no colors show

--- app/src/main/java/com/example/colorchooser/MainActivity.kt
+++ app/src/main/java/com/example/colorchooser/MainActivity.kt
@@ -1,21 +1,29 @@
-package com.example.colorpicker
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.colorchooser.ui.theme.ColorChooserTheme
 
@@ -40,11 +48,28 @@ class MainActivity : ComponentActivity() {
 fun ColorChooserApp(
     modifier: Modifier = Modifier,
 ) {
+
+    val colorsList = listOf("Blue", "Red", "Green")
+    var currentColor by remember { mutableStateOf("") }
+
+    val backgroundColor = when (currentColor) {
+        "Blue" -> Color.Blue
+        "Red" -> Color.Red
+        "Green" -> Color.Green
+        else -> Color.Gray
+    }
+
     Column(
         modifier = modifier
+            .background(backgroundColor)
+            .fillMaxWidth()
     ) {
-        ColorChooserMenu("Blue")
-        Text(text= "Blue")
+        ColorChooserMenu(
+            colorName= currentColor,
+            colorsList = colorsList,
+            setColor = {newColor -> currentColor = newColor}
+        )
+        Text(text= currentColor)
     }
 }
 
@@ -52,18 +77,44 @@ fun ColorChooserApp(
 @Composable
 fun ColorChooserMenu(
     colorName: String,
+    colorsList: List<String>,
+    setColor: (String) -> Unit,
     modifier: Modifier = Modifier
 ) {
+    var expanded by remember { mutableStateOf(false) }
+
+
     ExposedDropdownMenuBox(
-        expanded = false,
-        onExpandedChange = {}
+        expanded = expanded,
+        onExpandedChange = { expanded = !expanded },
+        modifier = modifier
+            .background(color = Color.LightGray)
     ) {
         OutlinedTextField(
             value = colorName,
             readOnly = true,
-            onValueChange = {}
+            onValueChange = {},
+            label = { Text("Chose a Color") },
+            trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+            modifier = modifier
+                .menuAnchor()
+                .fillMaxWidth()
         )
 
+        ExposedDropdownMenu(
+            expanded = expanded,
+            onDismissRequest = {expanded = false}
+        ) {
+            colorsList.forEach() { color ->
+                DropdownMenuItem(
+                    text = { Text(color) },
+                    onClick = {
+
+                        expanded = false
+                    }
+                )
+            }
+        }
     }
 }
 
@@ -74,6 +125,7 @@ fun ColorPickerPreview() {
         ColorChooserApp(
             Modifier
                 .fillMaxSize()
-                .wrapContentSize(Alignment.Center))
+                .wrapContentSize(Alignment.Center)
+        )
     }
 }
